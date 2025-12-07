744a240e87bcb447b42daf4f333abbc9cbcc455f
Milad-aghdam <miladagdam@gmail.com>
2025-12-07 19:11:07 +0330

    disable gradio share link for docker compatibility

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Kavosh-AI") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(share=True, debug=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
